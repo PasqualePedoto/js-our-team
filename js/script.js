fd35fdecccca294b6f66da752bd99d25cc1b67cc
PasqualePedoto <pasquale.pedoto123@gmail.com>
2022-07-05 15:17:34 +0200

    MILESTONE 0

---
+++ js/script.js
@@ -0,0 +1,69 @@
+// $ CONSEGNA
+
+// # MILESTONE 0:
+// Creare l’array di oggetti con le informazioni fornite.
+
+// # MILESTONE 1:
+// Stampare su console le informazioni di nome, ruolo e la stringa della foto
+
+// # MILESTONE 2:
+// Stampare le stesse informazioni su DOM sottoforma di stringhe
+
+// # BONUS 1:
+// Trasformare la stringa foto in una immagine effettiva (le trovate in allegato)
+
+// # BONUS 2:
+// Organizzare i singoli membri in card/schede e ispirarci alle grafiche in allegato
+
+
+// * DATI
+
+// Wayne Barnett |	Founder & CEO        |	wayne-barnett-founder-ceo.jpg
+// Angela Caroll |	Chief Editor         |	angela-caroll-chief-editor.jpg
+// Walter Gordon |	Office Manager       |	walter-gordon-office-manager.jpg
+// Angela Lopez  |	Social Media Manager |	angela-lopez-social-media-manager.jpg
+// Scott Estrada |	Developer            |	scott-estrada-developer.jpg
+// Barbara Ramos |	Graphic Designer     |	barbara-ramos-graphic-designer.jpg 
+
+// $ --------------------------------------------------
+// $ SVOLGIMENTO
+// $ --------------------------------------------------
+
+// # MILESTONE 1
+
+// * Creiamo un array contenente tutti i dati all'interno
+
+const teamMembers = [
+    {
+        "fullName": 'Wayne Barnett',
+        "role": 'Founder & CEO',
+        "stringPhoto": 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        "fullName": 'Angela Caroll',
+        "role": 'Chief Editor',
+        "stringPhoto": 'angela-caroll-chief-editor.jpg'
+    },
+    {
+        "fullName": 'Walter Gordon',
+        "role": 'Office Manager',
+        "stringPhoto": 'walter-gordon-office-manager.jpg'
+    },
+    {
+        "fullName": 'Angela Lopez',
+        "role": 'Social Media Manager',
+        "stringPhoto": 'angela-lopez-social-media-manager.jpg'
+    },
+    {
+        "fullName": 'Scott Estrada',
+        "role": 'Developer',
+        "stringPhoto": 'scott-estrada-developer.jpg'
+    },
+    {
+        "fullName": 'Barbara Ramos',
+        "role": 'Graphic Designer',
+        "stringPhoto": 'barbara-ramos-graphic-designer.jpg '
+    },
+];
+
+
